964068bacf6857765b61091de15ff47725ed567c
Rod <48662639+rodwestmoreland@users.noreply.github.com>
2021-03-03 20:28:33 -0500

    added StudentController and email model... again

---
+++ SchoolOfHardKnocks.Data/Email.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolOfHardKnocks.Data
+{
+    public class Email
+    {
+        [Key]
+        public int EmailId { get; set; }
+        [Required]
+        public string From { get; set; }
+        [Required]
+        public string To { get; set; }
+        [Required]
+        public DateTimeOffset CreateUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
