5817c6b8acdf8dc5307797ade906a28ed90cb6b3
Bob Madison <robertdavidmadison@gmail.com>
2021-03-04 10:06:22 -0500

    FixKeys

--- SchoolOfHardKnocks.Data/Faculty.cs
+++ SchoolOfHardKnocks.Data/Faculty.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace SchoolOfHardKnocks.Data
 {
     public class Faculty : SchoolName
     {
+        [Key]
         public Guid FacultyId { get; set; }
 
         public string Dept { get; set; }
